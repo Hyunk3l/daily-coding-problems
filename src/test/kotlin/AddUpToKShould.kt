94f771ccb8c8425e98787dfa3335f4a069926ef1
Fabri Di Napoli <fabrizio.dinapoli@scmspain.com>
2018-09-25 23:37:36 +0200

    Remove useless text

--- src/test/kotlin/AddUpToKShould.kt
+++ src/test/kotlin/AddUpToKShould.kt
@@ -6,9 +6,7 @@ import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
 /**
- * Good morning! Here's your coding interview problem for today.
-
-    This problem was recently asked by Google.
+    Good morning! Here's your coding interview problem for today.
 
     Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
 
@@ -16,7 +14,6 @@ import kotlin.test.assertTrue
 
     Bonus: Can you do this in one pass?
  */
-
 class AddUpToKShould {
 
     @Test
